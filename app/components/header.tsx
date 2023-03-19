9eb745b3786091e290431156ed29187e0d702c3d
Gan Wei Lun <ganweilun@Gans-MacBook-Pro-2.local>
2023-03-19 15:18:37 +0800

    CSS changes for header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon, MoonIcon } from "@heroicons/react/24/outline";
 export default function Header() {
   return (
     <div className="bg-white border-[#141A1A] shadow-sm border-b">
-      <div className="flex flex-row justify-between mx-auto max-w-7xl px-4 h-[75px] sm:px-6 lg:px-8">
+      <div className="flex flex-row justify-between mx-auto max-w-7xl px-6 h-[75px] sm:px-6 lg:px-[100px]">
         <div className="flex flex-shrink-0 items-center">
           <img
             className="block h-8 w-auto lg:hidden"
@@ -24,7 +24,7 @@ export default function Header() {
           <div className="relative text-gray-400 focus-within:text-gray-600">
             <input
               id="search"
-              className="block w-full rounded-md border-0 bg-gray-200 py-1.5 pl-2 pr-3 text-gray-900 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full h-[48px] rounded-md border-0 bg-gray-200 py-1.5 px-4 text-gray-900 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Search"
               type="search"
               name="search"
