6a5a7b5f11471ee529ed4bead01b08b338e1c571
Gan Wei Lun <ganweilun@Gans-MacBook-Pro-2.local>
2023-03-28 01:29:01 +0800

    Added colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "primary-green": "#4DBBBA",
+      "accent-yellow": "#FFC224",
+      "headings-black": "#141A1A",
+      "subheadings-black": "#263130",
+      "text-color": "#4C5757",
+      "placeholder-color": "#AFB6B6",
+      "white-95": "#F2F2F2",
+      "background-white": "#FCFCFC",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
