bb408f3e3b9b829dd6a56229fdbfebe8608f7e65
Wei Lun <garyganweilun@gmail.com>
2023-03-31 13:56:59 +0800

    Some css changes

--- app/components/ticker.tsx
+++ app/components/ticker.tsx
@@ -9,12 +9,6 @@ export default function Ticker() {
           <span className="text-sm mx-6">Total NFTs: 21,000</span>
           <span className="text-sm mx-6">24h NFT Volume: $137,000</span>
           <span className="text-sm mx-6">24h Top Sale: 1800<sup>ZIL</sup></span>
-          {/* <span className="text-sm mx-6">ZIL $0.025</span>
-          <span className="text-sm mx-6">Total Marketplaces 3</span>
-          <span className="text-sm mx-6">Total Collections 1200</span>
-          <span className="text-sm mx-6">Total NFTs 21,000</span>
-          <span className="text-sm mx-6">24h NFT Volume $137,000</span>
-          <span className="text-sm mx-6">24h Top Sale 1800<sup>ZIL</sup></span> */}
         </div>
         <div className="absolute top-0 py-2 animate-marquee2 whitespace-nowrap text-headings-black font-DM-Sans font-bold">
           <span className="text-sm mx-6">ZIL: $0.025</span>
@@ -23,16 +17,10 @@ export default function Ticker() {
           <span className="text-sm mx-6">Total NFTs: 21,000</span>
           <span className="text-sm mx-6">24h NFT Volume: $137,000</span>
           <span className="text-sm mx-6">24h Top Sale: 1800<sup>ZIL</sup></span>
-          {/* <span className="text-sm mx-6">ZIL $0.025</span>
-          <span className="text-sm mx-6">Total Marketplaces 3</span>
-          <span className="text-sm mx-6">Total Collections 1200</span>
-          <span className="text-sm mx-6">Total NFTs 21,000</span>
-          <span className="text-sm mx-6">24h NFT Volume $137,000</span>
-          <span className="text-sm mx-6">24h Top Sale 1800ZIL</span> */}
         </div>
       </div>
       <div className="hidden relative 2xl:flex overflow-x-hidden">
-      <div className="py-4 px-6 gap-[81px] w-screen h-[81px] flex flex-row justify-between items-center whitespace-nowrap text-headings-black font-bold font-DM-Sans">
+      <div className="py-4 px-[100px] gap-[81px] w-screen h-[81px] flex flex-row justify-between items-center whitespace-nowrap text-headings-black font-bold font-DM-Sans">
           <span className="text-sm mx-6">ZIL: $0.025</span>
           <span className="text-sm mx-6">Total Marketplaces: 3</span>
           <span className="text-sm mx-6">Total Collections: 1200</span>
