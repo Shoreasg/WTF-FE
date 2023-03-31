472266373f8591f02a6c5507af45f5f06a483ba5
Wei Lun <garyganweilun@gmail.com>
2023-03-31 14:04:08 +0800

    Change gradient percentage

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="h-[783px] 2xl:h-[647.31px] w-screen bg-gradient-to-b from-[#85D0D0] via-[#A4D2D1] to-[#4DBBBA]">
+      <div className="h-[783px] 2xl:h-[647.31px] w-screen bg-gradient-to-b from-[#85D0D0] via-[#A4D2D1_60%] to-[#4DBBBA]">
       <Ticker />
       </div>
     </>
